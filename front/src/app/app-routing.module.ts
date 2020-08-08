79fe9f4c9b81d549265d491bd40240d519ce480a
EliasBarret <elias-barreto@outlook.com>
2020-08-08 16:18:03 -0300

    Criacao dos componentes anguar

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { TaskNewComponent } from './task-new/task-new.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'task',
+    component: TasksComponent,
+    data: { title: 'Tarefas' }
+  },
+  {
+    path: 'task-detail/:id',
+    component: TaskDetailComponent,
+    data: { title: 'Detalhe Tarefa' }
+  },
+  {
+    path: 'task-new',
+    component: TaskNewComponent,
+    data: { title: 'Adicionar Tarefa' }
+  },
+  {
+    path: 'task-edit/:id',
+    component: TaskEditComponent,
+    data: { title: 'Editar o Tarefa' }
+  },
+  { path: '',
+    redirectTo: '/tasks',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { TaskNewComponent } from './task-new/task-new.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { HttpClientModule } from '@angular/common/http';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskDetailComponent,
+    TaskNewComponent,
+    TaskEditComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,  
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule, 
+    MatSelectModule,
+    MatSidenavModule,  
+    MatTableModule,
+    MatToolbarModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
